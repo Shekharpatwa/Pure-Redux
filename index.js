03fc9804698cb1d511936d81a1c240e1eec99afc
ShekharPatwa <shekharpatwa2001@gmail.com>
2023-07-12 22:37:17 +0530

    Logger and Action creators

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+
+const incre= "increment";
+const dec = 'decrement';
+const increByAmount = "incrementByAmount";
 
 //store
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(logger.default));
+
+const history =[];
+
+
 
 function reducer(state={amount:1},action){
+    if(action.type ===incre){
+        //immutability
+        return {amount:state.amount+1};
+    }
+    if(action.type ===dec){
+        //immutability
+        return {amount:state.amount-1};
+    }
+    if(action.type ===increByAmount){
+        //immutability
+        return {amount:state.amount + action.payload};
+    }
     return state;
 }
 
-//global state
+//Action creators
+function increment(){
+    return {type:incre};
+}
 
-console.log(store.getState())
+function decrement(){
+    return {type:dec}
+}
+
+function incrementByAmount(value){
+    return {type:increByAmount,payload:value}
+}
+
+// setInterval(() => {
+//     store.dispatch({type:'incrementByAmount',payload:4})
+// }, 5000);
+
+setInterval(() => {
+    store.dispatch(incrementByAmount(2));
+}, 2000);
+
+// store.subscribe(()=>{
+//     history.push(store.getState());
+//     console.log(history);
+// });
+
+// console.log(store.getState());
