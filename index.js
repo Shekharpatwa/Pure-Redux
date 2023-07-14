19cbe0e77c252830a0da9b50dfd21ff71ea55b96
ShekharPatwa <shekharpatwa2001@gmail.com>
2023-07-14 23:12:41 +0530

    done

--- index.js
+++ index.js
@@ -1,34 +1,86 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import logger from 'redux-logger';
+import axios from 'axios';
+import thunk from 'redux-thunk';
 
-const incre= "increment";
-const dec = 'decrement';
-const increByAmount = "incrementByAmount";
+const init = 'account/init';
+const incre= "account/increment";
+const dec = 'account/decrement';
+const increByAmount = "account/incrementByAmount";
+const getAccUserPending = 'account/getUser/pending';
+const getAccUserFulfilled = 'account/getUser/fulfilled';
+const getAccUserRejected = 'account/getUser/rejected';
+const incBonus = 'bonus/increment';
 
 //store
-const store = createStore(reducer,applyMiddleware(logger.default));
+const store = createStore(combineReducers({
+    account:accountReducer,
+    bonus:bonusReducer
+}),applyMiddleware(logger.default,thunk.default));
 
 const history =[];
 
 
 
-function reducer(state={amount:1},action){
-    if(action.type ===incre){
-        //immutability
-        return {amount:state.amount+1};
+function accountReducer(state={points:1},action){
+
+    switch(action.type){
+        case getAccUserFulfilled:
+            return {amount:action.payload,pending:false};
+        
+        case getAccUserRejected:
+            return {...state,error:action.error,pending:false};
+
+        case getAccUserPending:
+            return {...state,pending:true};
+
+        case incre:
+            return {amount:state.amount+1};
+
+        case dec:
+            return {amount:state.amount-1};
+
+        case increByAmount:
+            return {amount:state.amount + action.payload};
+
+        default:
+            return state;
     }
-    if(action.type ===dec){
-        //immutability
-        return {amount:state.amount-1};
+}
+
+function bonusReducer(state={points:0},action){
+    switch(action.type){
+        case incBonus:
+            return {points:state.points+1};
+
+        case increByAmount:
+            if(action.payload >=100)
+                return {points:state.points+1};
+        
+        default:
+            return state;
     }
-    if(action.type ===increByAmount){
-        //immutability
-        return {amount:state.amount + action.payload};
-    }
-    return state;
 }
 
 //Action creators
+
+function getUserAccount(id){
+    return async (dispatch,getState)=>{
+        try{
+            dispatch(getAccountUserPending())
+            const {data} = await axios.get(`http://localhost:3000/accounts/${id}`)
+            dispatch(getAccountUserFulfilled(data.amount));
+        }
+        catch(error){
+            dispatch(getAccountUserRejected(error.message));
+        }
+    }
+}
+
+function initUser(value){
+    return {type:getAccUserFulfilled, payload:value};
+}
+
 function increment(){
     return {type:incre};
 }
@@ -41,12 +93,30 @@ function incrementByAmount(value){
     return {type:increByAmount,payload:value}
 }
 
+function incrementBonus(){
+    return {type:incBonus}
+}
+
+function getAccountUserFulfilled(value){
+    return {type:getAccUserFulfilled, payload:value};
+}
+
+function getAccountUserPending(value){
+    return {type:getAccUserPending, payload:value};
+}
+
+function getAccountUserRejected(error){
+    return {type:getAccUserRejected, error:error};
+}
+
 // setInterval(() => {
 //     store.dispatch({type:'incrementByAmount',payload:4})
 // }, 5000);
 
-setInterval(() => {
-    store.dispatch(incrementByAmount(2));
+setTimeout(() => {
+    store.dispatch(getUserAccount(2));
+    // store.dispatch(incrementByAmount(2));
+    // store.dispatch(incrementBonus());
 }, 2000);
 
 // store.subscribe(()=>{
@@ -55,3 +125,12 @@ setInterval(() => {
 // });
 
 // console.log(store.getState());
+
+//Asyc api call
+
+async function apiCall(){
+    const {data} = await axios.get('http://localhost:3000/accounts/1')
+    console.log(data);
+}
+
+// apiCall();
---
+++ test.js
@@ -0,0 +1,3 @@
+const state ={account :{amount :1},bonus:{points:2}}
+const newState ={account :{...state.account},bonus:{points:state.bonus.points+1}}
+console.log(newState,state);
